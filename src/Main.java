ded816fec162b972db9a552d315569bd26de1763
Gautam Kumar <116151957+Gautamkumarmaurya@users.noreply.github.com>
2025-08-07 19:51:03 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,50 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Library library = new Library();
+        Scanner scanner = new Scanner(System.in);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        // Sample data
+        library.addBook(new Book(1, "Java Basics", "James Gosling"));
+        library.addBook(new Book(2, "OOPS Concepts", "Bjarne Stroustrup"));
+
+        library.registerUser(new User(1001, "Gautam"));
+        library.registerUser(new User(1002, "Raunak"));
+
+        while (true) {
+            System.out.println("\n==== Library Menu ====");
+            System.out.println("1. View Available Books");
+            System.out.println("2. Issue Book");
+            System.out.println("3. Return Book");
+            System.out.println("4. Exit");
+
+            int choice = scanner.nextInt();
+
+            switch (choice) {
+                case 1:
+                    library.displayAvailableBooks();
+                    break;
+                case 2:
+                    System.out.print("Enter User ID: ");
+                    int uid = scanner.nextInt();
+                    System.out.print("Enter Book ID: ");
+                    int bid = scanner.nextInt();
+                    library.issueBook(bid, uid);
+                    break;
+                case 3:
+                    System.out.print("Enter User ID: ");
+                    int uidr = scanner.nextInt();
+                    System.out.print("Enter Book ID: ");
+                    int bidr = scanner.nextInt();
+                    library.returnBook(bidr, uidr);
+                    break;
+                case 4:
+                    System.out.println("Exiting Library System...");
+                    return;
+                default:
+                    System.out.println("Invalid option.");
+            }
         }
     }
 }
