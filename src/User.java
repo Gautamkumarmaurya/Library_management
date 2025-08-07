817f6220735098f77c06952d625e6fcf63f44d71
Gautam Kumar <116151957+Gautamkumarmaurya@users.noreply.github.com>
2025-08-07 19:50:37 +0530

    Update User.java

--- src/User.java
+++ src/User.java
@@ -1,4 +1,38 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
+import java.util.List;
 
 public class User {
+    private int userId;
+    private String name;
+    private List<Book> borrowedBooks = new ArrayList<>();
+
+    public User(int userId, String name) {
+        this.userId = userId;
+        this.name = name;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void borrowBook(Book book) {
+        if (book.isAvailable()) {
+            borrowedBooks.add(book);
+            book.setAvailable(false);
+            System.out.println(name + " borrowed: " + book.getBookId());
+        } else {
+            System.out.println("Book not available.");
+        }
+    }
+
+    public void returnBook(Book book) {
+        if (borrowedBooks.remove(book)) {
+            book.setAvailable(true);
+            System.out.println(name + " returned: " + book.getBookId());
+        } else {
+            System.out.println("This user didn't borrow that book.");
+        }
+    }
 }
+
+
