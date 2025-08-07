891cfd13c2a458bdb5cbdfc50528526bd6740db5
Gautam Kumar <116151957+Gautamkumarmaurya@users.noreply.github.com>
2025-08-07 19:49:52 +0530

    Update Book.java

--- src/Book.java
+++ src/Book.java
@@ -1,4 +1,30 @@
-package PACKAGE_NAME;
 
 public class Book {
+    private int bookId;
+    private String title;
+    private String author;
+    private boolean isAvailable = true;
+
+    public Book(int bookId, String title, String author) {
+        this.bookId = bookId;
+        this.title = title;
+        this.author = author;
+    }
+
+    public int getBookId() {
+        return bookId;
+    }
+
+    public boolean isAvailable() {
+        return isAvailable;
+    }
+
+    public void setAvailable(boolean available) {
+        isAvailable = available;
+    }
+
+    public void displayDetails() {
+        System.out.println("ID: " + bookId + ", Title: " + title + ", Author: " + author + ", Available: " + isAvailable);
+    }
 }
+
